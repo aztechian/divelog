f5ae88d72fd77c20384fc0b8c214143cb2c90be1
ian <ian@51321470-acf5-0310-bcb5-c959670cf2a8>
2005-05-04 06:27:08 +0000

    First revision with an HTML template to work with. Added shell.html in the templates folder, and many existing items then needed modifications. Added first images.
    git-svn-id: svn://chernobyl.imartin.net/divelog/trunk@6 51321470-acf5-0310-bcb5-c959670cf2a8

--- site/common.php
+++ site/common.php
@@ -29,36 +29,36 @@ class Smarty_divelog extends Smarty {
 } //End of class Smarty_divelog
 
 class pg_db {
-	var $handle = false; //database handle for use in functions
-	var $resultSet; //used to hold temporary results (see getRow)
-	var $row;
-	var $rows = array ();
+	private $handle = false; //database handle for use in functions
+	private $resultSet; //used to hold temporary results (see getRow)
+	private $row;
+	private $rows = array ();
 
-	/*========================================================================
-	 * FUNCTION: pg_db class constructor
-	 *
-	 *         Input:  None
-	 *        Output:  None
-	 *       Returns:  A new pg_db object
-	 *   Limitations:  Does not initialize the class variables
-	 *========================================================================
-	 */
 	function pg_db() {
-
+		/*========================================================================
+		 * FUNCTION: pg_db class constructor
+		 *
+		 *         Input:  None
+		 *        Output:  None
+		 *       Returns:  A new pg_db object
+		 *   Limitations:  Does not initialize the class variables
+		 *========================================================================
+		 */
 	}
-	/*========================================================================
-	 * FUNCTION: connect()
-	 *
-	 *         Input:  None
-	 *        Output:  None if successful. Upon failure an error
-	 *                 message will be echoed.
-	 *       Returns:  a boolean that represents the state
-	 *                 of the connection to the database.
-	 *                 true is success. false is failure. 
-	 *   Limitations:  None
-	 *========================================================================
-	 */
+
 	function connect() {
+		/*========================================================================
+		 * FUNCTION: connect()
+		 *
+		 *         Input:  None
+		 *        Output:  None if successful. Upon failure an error
+		 *                 message will be echoed.
+		 *       Returns:  a boolean that represents the state
+		 *                 of the connection to the database.
+		 *                 true is success. false is failure. 
+		 *   Limitations:  None
+		 *========================================================================
+		 */
 		include ('config.php');
 		$db = pg_connect("host=".$dbconf['host']." port=".$dbconf['port']." user=".$dbconf['name']." password=".$dbconf['pass']." dbname=".$dbconf['db']);
 
@@ -68,21 +68,22 @@ class pg_db {
 		$this->handle = $db;
 		return true;
 	}
-	/*========================================================================
-	 * FUNCTION: query()
-	 *
-	 *         Input:  a string that holds a valid postgreSQL SQL query.
-	 *        Output:  None on successful query execution. Upon failure of
-	 *                 query execution, appropriate error messages will be echoed.
-	 *       Returns:  A boolean that represents the success of execution of the
-	 *                 given query string.
-	 *                 true is success. false is failure.
-	 *   Limitations:  Error messages may not correspond to the actual error.
-	 *                 See the PHP documentation for the pg_last_error function
-	 *                 for details.
-	 *========================================================================
-	 */
+
 	function query($qstr) {
+		/*========================================================================
+		 * FUNCTION: query()
+		 *
+		 *         Input:  a string that holds a valid postgreSQL SQL query.
+		 *        Output:  None on successful query execution. Upon failure of
+		 *                 query execution, appropriate error messages will be echoed.
+		 *       Returns:  A boolean that represents the success of execution of the
+		 *                 given query string.
+		 *                 true is success. false is failure.
+		 *   Limitations:  Error messages may not correspond to the actual error.
+		 *                 See the PHP documentation for the pg_last_error function
+		 *                 for details.
+		 *========================================================================
+		 */
 		if (!$this->handle) {
 			return $this->problem("No database connection for query.");
 		} else {
@@ -93,56 +94,59 @@ class pg_db {
 			return true;
 		}
 	}
-	/*========================================================================
-	 * FUNCTION: rows()
-	 *
-	 *  Precondition:  A result set must exist (ie. a call to query())
-	 *         Input:  None, takes existing result set
-	 *        Output:  Upon failure, appropriate error messages are echoed.
-	 *       Returns:  Mixed. Returns an integer of the number of rows in the
-	 *                 result set if successful. A failure will return a boolean
-	 *                 of false.
-	 *   Limitations:  None.
-	 *========================================================================
-	 */
+
 	function rows() {
+		/*========================================================================
+		 * FUNCTION: rows()
+		 *
+		 *  Precondition:  A result set must exist (ie. a call to query())
+		 *         Input:  None, takes existing result set
+		 *        Output:  Upon failure, appropriate error messages are echoed.
+		 *       Returns:  Mixed. Returns an integer of the number of rows in the
+		 *                 result set if successful. A failure will return a boolean
+		 *                 of false.
+		 *   Limitations:  None.
+		 *========================================================================
+		 */
 		if ($this->resultSet) {
 			return pg_num_rows($this->resultSet);
 		} else {
 			return $this->problem("Can't give number of rows without a result set");
 		}
 	}
-	/*========================================================================
-	 * FUNCTION: cols()
-	 *
-	 *  Precondition:  A result set must exist (ie. a call to query())
-	 *         Input:  None, takes existing result set.
-	 *        Output:  Upon failure, appropriate error messages are echoed.
-	 *       Returns:  Mixed. Returns an integer of the number of columns in the
-	 *                 result set if successful. A failure will return a boolean
-	 *                 of false.
-	 *   Limitations:  None.
-	 *========================================================================
-	 */
+
 	function cols() {
+		/*========================================================================
+		 * FUNCTION: cols()
+		 *
+		 *  Precondition:  A result set must exist (ie. a call to query())
+		 *         Input:  None, takes existing result set.
+		 *        Output:  Upon failure, appropriate error messages are echoed.
+		 *       Returns:  Mixed. Returns an integer of the number of columns in the
+		 *                 result set if successful. A failure will return a boolean
+		 *                 of false.
+		 *   Limitations:  None.
+		 *========================================================================
+		 */
 		if ($this->resultSet) {
 			return pg_num_cols($this->resultSet);
 		} else {
 			return $this->problem("Can't give number of columns without a result set");
 		}
 	}
-	/*========================================================================
-	 * FUNCTION: getAllRows()
-	 *
-	 *  Precondition:  A result set must exist (ie. a call to query())
-	 *         Input:  None, takes existing result set.
-	 *        Output:  Echoes error messages upon failure.
-	 *       Returns:  An array holding the rows of the previous query results.
-	 *                 Returns a boolean of false upon failure.
-	 *   Limitations:  None.
-	 *========================================================================
-	 */
+
 	function getAllRows() {
+		/*========================================================================
+		 * FUNCTION: getAllRows()
+		 *
+		 *  Precondition:  A result set must exist (ie. a call to query())
+		 *         Input:  None, takes existing result set.
+		 *        Output:  Echoes error messages upon failure.
+		 *       Returns:  An array holding the rows of the previous query results.
+		 *                 Returns a boolean of false upon failure.
+		 *   Limitations:  None.
+		 *========================================================================
+		 */
 		if ($this->resultSet) {
 			$this->rows = NULL;
 			$tempRow = '';
@@ -155,19 +159,19 @@ class pg_db {
 		}
 	}
 
-	/*========================================================================
-	 * FUNCTION: getSingleRow()
-	 *
-	 *  Precondition:  A result set must exist (ie. a call to query())
-	 *         Input:  None, takes existing result set.
-	 *        Output:  Echoes error messages upon failure.
-	 *       Returns:  An associative array holding the values of one row (the
-	 *                 next in the result set). Returns a boolean value of 
-	 *                 false upon failure.
-	 *   Limitations:  None.
-	 *========================================================================
-	 */
 	function getSingleRow() {
+		/*========================================================================
+		 * FUNCTION: getSingleRow()
+		 *
+		 *  Precondition:  A result set must exist (ie. a call to query())
+		 *         Input:  None, takes existing result set.
+		 *        Output:  Echoes error messages upon failure.
+		 *       Returns:  An associative array holding the values of one row (the
+		 *                 next in the result set). Returns a boolean value of 
+		 *                 false upon failure.
+		 *   Limitations:  None.
+		 *========================================================================
+		 */
 		if ($this->resultSet) {
 			$this->row = pg_fetch_array($this->resultSet);
 			return $this->row;
@@ -176,81 +180,85 @@ class pg_db {
 		}
 	}
 
-	/*========================================================================
-	 * FUNCTION: finish()
-	 *
-	 *  Precondition:  A result set must exist (ie. a call to query())
-	 *         Input:  None, takes existing result set.
-	 *        Output:  None
-	 *       Returns:  A boolean indicating the success of freeing memory taken
-	 *                 by the most recent result set.
-	 *   Limitations:  None.
-	 *========================================================================
-	 */
 	function finish() {
+		/*========================================================================
+		* FUNCTION: finish()
+		*
+		*  Precondition:  A result set must exist (ie. a call to query())
+		*         Input:  None, takes existing result set.
+		*        Output:  None
+		*       Returns:  A boolean indicating the success of freeing memory taken
+		*                 by the most recent result set.
+		*   Limitations:  None.
+		*========================================================================
+		*/
 		return pg_free_result($this->resultSet);
 	}
-	/*========================================================================
-	 * FUNCTION: queryAllRows($qstr)
-	 *
-	 *         Input:  A string holding the postgreSQL SQL query to be executed.
-	 *        Output:  None
-	 *       Returns:  An array containing the rows of the result set from the
-	 *                 given query.
-	 *   Limitations:  None
-	 *========================================================================
-	 */
+
 	function queryAllRows($qstr) {
+		/*========================================================================
+		* FUNCTION: queryAllRows($qstr)
+		*
+		*         Input:  A string holding the postgreSQL SQL query to be executed.
+		*        Output:  None
+		*       Returns:  An array containing the rows of the result set from the
+		*                 given query.
+		*   Limitations:  None
+		*========================================================================
+		*/
 		$this->query($qstr);
 		$tempSet = $this->getAllRows();
 		pg_free_result($this->resultSet);
 		return $tempSet;
 	}
-	/*========================================================================
-	 * FUNCTION: queryRow($qstr)
-	 *
-	 *         Input:  A string containing the postgreSQL SQL query to execute.
-	 *        Output:  None
-	 *       Returns:  An associative array holding the values of a single row
-	 *                 (the next in the result set) from the results of the 
-	 *                 query passed in.
-	 *   Limitations:  None
-	 *========================================================================
-	 */
+
 	function queryRow($qstr) {
+		/*========================================================================
+		* FUNCTION: queryRow($qstr)
+		*
+		*         Input:  A string containing the postgreSQL SQL query to execute.
+		*        Output:  None
+		*       Returns:  An associative array holding the values of a single row
+		*                 (the next in the result set) from the results of the 
+		*                 query passed in.
+		*   Limitations:  None
+		*========================================================================
+		*/
 		$this->query($qstr);
 		$tempSet = $this->getSingleRow();
 		pg_free_result($this->resultSet);
 		return $tempSet;
 	}
-	/*========================================================================
-	 * FUNCTION: fullQuery($qstr)
-	 *
-	 *         Input:  A string containing a postgreSQL SQL query to execute.
-	 *        Output:  None
-	 *       Returns:  A boolean containing the status of freeing the memory
-	 *                 taken by the result set from the query that was passed in.
-	 *   Limitations:  Does not return the status of the query itself.
-	 *========================================================================
-	 */
+
 	function fullQuery($qstr) {
+		/*========================================================================
+		* FUNCTION: fullQuery($qstr)
+		*
+		*         Input:  A string containing a postgreSQL SQL query to execute.
+		*        Output:  None
+		*       Returns:  A boolean containing the status of freeing the memory
+		*                 taken by the result set from the query that was passed in.
+		*   Limitations:  Does not return the status of the query itself.
+		*========================================================================
+		*/
 		$this->query($qstr);
 		return pg_free_result($this->resultSet);
 	}
-	/*========================================================================
-	 * FUNCTION: $this->problem()
-	 *
-	 *         Input:  A string holding the error message to be printed.
-	 *        Output:  The given string parameter plus any error messages that
-	 *                 can be had from pg_last_error.
-	 *       Returns:  a boolean of false. This function is called in an error
-	 *                 state, thus it always returns false to indicate this
-	 *                 and for ease of use by the calling function. This
-	 *                 function can be returned directly from the calling function.
-	 *   Limitations:  Always returns false.
-	 *========================================================================
-	 */
+
 	function problem($txt) {
+		/*========================================================================
+		* FUNCTION: $this->problem()
+		*
+		*         Input:  A string holding the error message to be printed.
+		*        Output:  The given string parameter plus any error messages that
+		*                 can be had from pg_last_error.
+		*       Returns:  a boolean of false. This function is called in an error
+		*                 state, thus it always returns false to indicate this
+		*                 and for ease of use by the calling function. This
+		*                 function can be returned directly from the calling function.
+		*   Limitations:  Always returns false.
+		*========================================================================
+		*/
 		echo pg_last_error()."\n";
 		echo "$txt"."\n\n";
 		return false;
@@ -261,4 +269,6 @@ class pg_db {
 $db = new pg_db(); //instantiate new db object for simplicity of "include-er"
 $db->connect();
 ?>
+
+
 
--- site/index.php
+++ site/index.php
@@ -1,33 +1,25 @@
 <?PHP
+include ('common.php');
 
-include('common.php');
+if (isset ($_POST['action']) AND strtoupper($_POST['action']) == "LOGIN") {
 
-if( isset($_POST['action']) AND strtoupper($_POST['action']) == "LOGIN" ){
+	$userid = $db->getSingleRow("SELECT userid,password FROM users WHERE username='".addslashes($_POST['user'])."'");
 
-   $userid = $db->getAllRows("SELECT userid,password FROM users WHERE username='".addslashes($_POST['user'])."'" );
-   
-   if( $userid[0]['password'] == md5($_POST['pass']) ){
+	if ($userid[0]['password'] == md5($_POST['pass'])) {
+		$db->fullQuery("UPDATE users SET lastvisit='NOW', loggedin='true' WHERE userid='".$userid[0]['userid']."'");
+		header('Location: welcome.php');
+		die('');
+	} else {
+		header('Location: index.php');
+		die('');
+	}
+} else {
 	$stpl = new Smarty_divelog();
-        $stpl->assign('title', "Divelog Login");
-	$stpl->assign('name',$_POST['user']);
-	$stpl->assign('uid',$userid[0]['userid']);
-	$stpl->assign('lastlogin',time());
+	$stpl->assign('title', "Divelog Login");
+	$content = $stpl->fetch('index.html'); //get and parse the main "content" of the page
 
-	$db->getAllRows("UPDATE users SET lastvisit='NOW', loggedin='true' WHERE userid='".$userid[0]['userid']."'");
-
-	$stpl->display('welcome.html');
-	die('');
-   }
-   else{
-	$stpl = new Smarty_divelog();
-        $stpl->assign('title', "Divelog Login");
-	$stpl->display('index.html');
-	die('');
-   }
-}
-else{
-   $stpl = new Smarty_divelog();
-   $stpl->assign('title', "Divelog Login");
-   $stpl->display('index.html');
+	$stpl->assign('content', $content); //this puts the "content" of the page into the main layout template
+	$stpl->display('shell.html');
 }
 ?>
+
